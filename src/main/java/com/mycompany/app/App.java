46fa968dd49110e67a854267ce863755ea4f3845
RafiAwsDevops <rafiawsdevops1@gmail.com>
2025-12-10 10:11:18 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,13 @@
 package com.mycompany.app;
 
-public class App {
-    public String greet() {
-        return "Hello SonarCloud!";
-    }
-
-    public static void main(String[] args) {
-        App app = new App();
-        System.out.println(app.greet());
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
